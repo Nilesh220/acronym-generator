c83f1e75978701e12fb6f96c7a43d726f9940f57
PRATHAMESH PATEL <114341521+prathameshpatelmumbai@users.noreply.github.com>
2022-12-10 19:29:23 +0530

    Update acronym generator.py

--- acronym generator.py
+++ acronym generator.py
@@ -1,7 +1,7 @@
 # Taking in put from user
-user_input = str(input("Enter a Sentence: "))
+user_inpu = str(input("Enter a Sentence: "))
 # making list of words
-text = user_input.split()
+text = user_inpu.split()
 # Creating empty string
 a = " "
 # Acessing each word one by one using for loop
